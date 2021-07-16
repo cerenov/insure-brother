411be05256ae53be724938f58f81a4956c6b3afa
Джангар Церенов <cerenov.d.v@gmail.com>
2021-07-16 21:23:23 +0300

    added models insurance and response, migrations

---
+++ insure_brother/database/migrations/2021_07_16_172516_create_responses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('insurances_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('mail');
+            $table->integer('number_months');
+            $table->decimal('cost', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
