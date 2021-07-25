57a9107e14a210ec8aa4a145f2872a83eb6162c3
Джангар Церенов <cerenov.d.v@gmail.com>
2021-07-25 20:44:16 +0300

    grouped routes

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 После того как проект будет развернут, нужно выполнить комманды: 
 ```sh
+docker exec -it insure_brother-app npm install
+docker exec -it insure_brother-app npm run dev
 docker exec -it insure_brother-app php artisan migrate
 ```
--- insure_brother/routes/web.php
+++ insure_brother/routes/web.php
@@ -17,16 +17,18 @@ use \App\Http\Controllers\ResponseController;
 
 Route::get('/', [InsuranceController::class, 'indexForClient'])->name('home');
 Route::get('/dashboard', [InsuranceController::class, 'index'])->middleware(['auth'])->name('dashboard');
-Route::get('/insurance/create', function () {
-    return view('new_insurance');
-})->middleware('auth')->name('create_insurance');
-Route::post('/insurance/create', [InsuranceController::class, 'create'])->middleware('auth')->name('create_insurance');
-Route::get('/insurance/{id}/read', [InsuranceController::class, 'read'])->middleware('auth')->name('read_insurance');
-Route::post('/insurance/{id}/update', [InsuranceController::class, 'update'])->middleware('auth')->name('update_insurance');
-Route::post('/insurance/{id}/delete', [InsuranceController::class, 'delete'])->middleware('auth')->name('delete_insurance');
 Route::get('/insurance/{id}/read-client', [InsuranceController::class, 'readForClient'])->name('read_insurance_for_client');
-
-Route::get('/responses', [ResponseController::class, 'index'])->middleware('auth')->name('index_response');
 Route::post('/response/create', [ResponseController::class, 'create'])->name('create_response');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/insurance/create', function () {
+        return view('new_insurance');
+    })->middleware('auth')->name('create_insurance');
+    Route::post('/insurance/create', [InsuranceController::class, 'create'])->middleware('auth')->name('create_insurance');
+    Route::get('/insurance/{id}/read', [InsuranceController::class, 'read'])->middleware('auth')->name('read_insurance');
+    Route::post('/insurance/{id}/update', [InsuranceController::class, 'update'])->middleware('auth')->name('update_insurance');
+    Route::post('/insurance/{id}/delete', [InsuranceController::class, 'delete'])->middleware('auth')->name('delete_insurance');
+    Route::get('/responses', [ResponseController::class, 'index'])->middleware('auth')->name('index_response');
+});
 
 require __DIR__ . '/auth.php';
